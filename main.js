34607949507f8d0ddf678493b93a0191d4f4cc78
shriya0511-max <76861018+shriya0511-max@users.noreply.github.com>
2021-05-25 10:43:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+     
+}
+function setup(){
+    canvas=createCanvas(640,480)
+    canvas.position(110,250)}
+
+function draw(){
+    
+    circle (50, 100, 100)
+    rect(100, 90, 500, 30)
+    circle(590, 435, 100)
+    rect(576, 150, 30, 235)
+    circle(590, 100, 100);
+    rect(100, 420, 440, 30)
+    circle(50, 430, 100);
+    rect(30, 150, 30, 230)
+}
